8c8d7521759b39806c4cb838a550813784c63d9a
FalseDev <thetechnopath1802@gmail.com>
2020-08-13 16:29:05 +0000

    Fix static serving

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
