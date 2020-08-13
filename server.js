00a5dd27da3379d3b57449dff4b263ab53469399
FalseDev <thetechnopath1802@gmail.com>
2020-08-13 16:37:43 +0000

    Serve index on unknown path

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
 });
